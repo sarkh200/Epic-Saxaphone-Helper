20d4a849cc474df4ff2acf6c7f9d45ab3b96458b
Sadra Saharkhiz <sarkh200@gmail.com>
2024-06-17 16:48:12 -0400

    made it use api insted of localtime

--- index.js
+++ index.js
@@ -17,28 +17,44 @@ function onYouTubeIframeAPIReady() {
 	});
 }
 
+async function getJSON(url) {
+	const response = await fetch(url);
+	return response.json();
+}
+
+async function getTime() {
+	let data = await getJSON("http://worldtimeapi.org/api/ip");
+	let time = data.datetime;
+	let date = new Date(time);
+	let hours = timeToStart.split(':')[0] - date.getHours();
+	let minutes = timeToStart.split(':')[1] - date.getMinutes();
+	let totalSeconds = ((hours * 360) + (minutes * 60)) - date.getSeconds();
+	return totalSeconds;
+}
+
+async function go() {
+	if (timeToStart != 0) {
+		let totalSeconds = await getTime();
+		let time = new Date();
+		output.innerHTML = `Will play in ${totalSeconds} seconds as of ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+		if (totalSeconds > 0) {
+			setTimeout(() => { player.playVideo() }, totalSeconds * 1000);
+		}
+		else {
+			player.seekTo(0);
+			player.playVideo();
+		}
+	}
+}
+
 let timeToStart = 0;
 
-onload = (event) => {
+onload = () => {
 	const startTimeSelector = document.getElementById("StartTimeSelector");
 	const goButton = document.getElementById("Go");
+	const output = document.getElementById("ouput");
 
-	goButton.addEventListener("click", (event) => {
-		if (timeToStart != 0) {
-			let date = new Date();
-			let hours = timeToStart.split(':')[0] - date.getHours();
-			let minutes = timeToStart.split(':')[1] - date.getMinutes();
-			let totalSeconds = ((hours * 360) + (minutes * 60)) - date.getSeconds();
-			console.log(`Will play in ${totalSeconds} seconds`)
-			if (totalSeconds > 0) {
-				setTimeout(() => { player.playVideo() }, totalSeconds * 1000);
-			}
-			else {
-				player.seekTo(0);
-				player.playVideo();
-			}
-		}
-	});
+	goButton.addEventListener("click", () => go());
 
 	startTimeSelector.addEventListener("change", (event) => {
 		timeToStart = event.target.value;
